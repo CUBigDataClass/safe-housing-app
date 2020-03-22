3e5830d8e8f24461bd0934ae6be273eed9ed0c6a
Veean Prasad <veenaprasad1995@gmail.com>
2020-03-21 23:57:38 -0600

    Redis-Client

---
+++ backend/config/settings.py
@@ -0,0 +1,25 @@
+import os
+import configparser
+
+class BaseConfig():
+   API_PREFIX = '/api'
+   TESTING = False
+   DEBUG = False
+
+   config = configparser.ConfigParser()
+
+   @staticmethod
+   def get_or_else(section, option, default_value):
+      if BaseConfig.config.has_option(section, option):
+         return BaseConfig.config.get(section, option,
+                                  fallback=default_value)
+      else:
+         return os.environ.get(section, option, )
+
+
+class DevConfig(BaseConfig):
+   REDIS_HOST = '104.154.59.49'
+   REDIS_DB = 0
+   REDIS_PORT = 6379
+
+
